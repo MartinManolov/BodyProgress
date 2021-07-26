ec34a67ae1c4f3f020c8c7dc6cd03460a315f0da
MartinManolov <mmmanolov@gmail.com>
2021-07-26 21:38:03 +0300

    Implement view component for body statistics.

---
+++ Web/BodyProgress.Web/ViewComponents/ProfileBodyStatisticsViewComponent.cs
@@ -0,0 +1,27 @@
+﻿namespace BodyProgress.Web.ViewComponents
+{
+    using System.Threading.Tasks;
+
+    using BodyProgress.Services.Contracts;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ProfileBodyStatisticsViewComponent : ViewComponent
+    {
+        private readonly IBodyStatisticsService bodyStatisticsService;
+        private readonly IUsersService usersService;
+
+        public ProfileBodyStatisticsViewComponent(IBodyStatisticsService bodyStatisticsService,
+             IUsersService usersService)
+        {
+            this.bodyStatisticsService = bodyStatisticsService;
+            this.usersService = usersService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string username)
+        {
+            var userId = this.usersService.GetIdByUsername(username);
+            var bodyStatistics = this.bodyStatisticsService.All(userId);
+            return this.View(bodyStatistics);
+        }
+    }
+}

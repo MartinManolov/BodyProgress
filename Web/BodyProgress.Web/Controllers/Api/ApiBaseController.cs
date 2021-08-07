7084baa3aeb3872854933198d764bd8b9a8c23dd
MartinManolov <mmmanolov@gmail.com>
2021-08-07 15:59:56 +0300

    Moved API controllers to a separate folder

---
+++ Web/BodyProgress.Web/Controllers/Api/ApiBaseController.cs
@@ -0,0 +1,9 @@
+﻿namespace BodyProgress.Web.Controllers.Api
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    public class ApiBaseController : BaseController
+    {
+    }
+}
---
+++ Web/BodyProgress.Web/Controllers/Api/LikesController.cs
@@ -0,0 +1,56 @@
+﻿namespace BodyProgress.Web.Controllers.Api
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using BodyProgress.Services.Contracts;
+    using BodyProgress.Web.ViewModels.ViewInputModels;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("/api/[controller]")]
+    [Authorize]
+    public class LikesController : ApiBaseController
+    {
+        private readonly ILikesService likesService;
+
+        public LikesController(ILikesService likesService)
+        {
+            this.likesService = likesService;
+        }
+
+        [HttpGet]
+        public IActionResult GetLikes(string postId)
+        {
+            var likesCount = this.likesService.GetByPostId(postId);
+
+            return this.Ok(likesCount);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody]LikeInputModel input)
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var likeId = await this.likesService.Add(userId, input.PostId);
+            if (likeId == null)
+            {
+                return this.StatusCode(409);
+            }
+
+            return this.CreatedAtAction("Add", likeId);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody]LikeInputModel input)
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var isDeleted = await this.likesService.Delete(userId, input.PostId);
+            if (isDeleted)
+            {
+                return this.StatusCode(204);
+            }
+
+            return this.StatusCode(406);
+        }
+    }
+}
